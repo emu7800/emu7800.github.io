76ddb65d58e6b6f162b46c59a798397868b6902f
Mike Murphy <mike@jones-murphy.org>
2014-05-22 13:11:22 -0700

    adjust version numbers to 1.9

--- EMU7800.D2D/EMU7800.D2D.Shell/WinRT_8.1WP/Properties/AssemblyInfo.cs
+++ EMU7800.D2D/EMU7800.D2D.Shell/WinRT_8.1WP/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("EMU7800")]
-[assembly: AssemblyDescription("An Atari 2600/7800 .NET-based Emulator for WinRT")]
+[assembly: AssemblyDescription("An Atari 2600/7800 .NET-based Emulator for WinRT Phone")]
 
 [assembly: CLSCompliant(false)]
 [assembly: ComVisible(false)]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("EMU7800 for WinRT")]
 [assembly: AssemblyCompany("Mike Murphy")]
 [assembly: AssemblyCopyright("Copyright © 2012-2014 Mike Murphy")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
