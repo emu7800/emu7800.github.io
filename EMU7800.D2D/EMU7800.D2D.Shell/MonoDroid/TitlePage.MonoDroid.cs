1745a8025e82875892972b0681079373d4bc02e7
Mike Murphy <mike@jones-murphy.org>
2015-05-07 05:01:18 -0700

    correct version number on title page remove some unneeded resources

--- EMU7800.D2D/EMU7800.D2D.Shell/MonoDroid/TitlePage.MonoDroid.cs
+++ EMU7800.D2D/EMU7800.D2D.Shell/MonoDroid/TitlePage.MonoDroid.cs
@@ -1,12 +1,15 @@
+// © Mike Murphy
+
+using EMU7800.MonoDroid;
+
 namespace EMU7800.D2D.Shell
 {
     public partial class TitlePage
     {
         static string GetVersionInfo()
         {
-            //var version = Windows.ApplicationModel.Package.Current.Id.Version;
-            //var versionInfo = string.Format("Version {0}.{1} (Core 1.4) {2}", version.Major, version.Minor, GetBuildConfiguration());
-            var versionInfo = "FIXME Version";
+            var pi = MainActivity.App.ApplicationContext.PackageManager.GetPackageInfo(MainActivity.App.ApplicationContext.PackageName, 0);
+            var versionInfo = string.Format("Version {0} (Core 1.4) {1}", pi.VersionName, GetBuildConfiguration());
             return versionInfo;
         }
     }
