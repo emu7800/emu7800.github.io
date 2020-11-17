07260b60bf1ebb4447f50a6632df7b85123b53b3
Mike Murphy <mike@jones-murphy.org>
2020-11-16 20:23:08 -0800

    remove unneeded classes

--- _NG/src/shell/Services/Dto/StringType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EMU7800.Services.Dto
-{
-    public class StringType
-    {
-        public string Line { get; set; } = string.Empty;
-
-        public static StringType ToStringType(string s) => new StringType { Line = s };
-    }
-}
