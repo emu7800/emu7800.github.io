960aaa7af4a2d3738cec7fbe1450dc8282a3556f
Mike Murphy <mike@jones-murphy.org>
2020-11-19 13:03:25 -0800

    eliminate launcher, now in shell

--- src/tools/Launcher/DropDownItem`1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EMU7800.Launcher
-{
-    public class DropDownItem<T> where T : new()
-    {
-        public string DisplayName { get; set; } = string.Empty;
-        public T Value { get; set; } = new T();
-    }
-}
