85eaf7f3f2db4bc3b66fb02d23ad5dd6ab12a7d8
Mike Murphy <mike@jones-murphy.org>
2020-11-19 15:11:28 -0800

    build script update

--- BuildArtifacts.ps1
+++ BuildArtifacts.ps1
@@ -4,18 +4,19 @@ Push-Location $PSScriptRoot
 Remove-Item -Recurse -Force .\artifacts
 
 # copy source artifacts
-robocopy.exe .\src\                      .\artifacts\srcdist\src\ *.cs *.csproj *.cpp *.h *.manifest *.sln  *.filters *.rc *.vcxproj *.ico *.txt *.png *.json /S /XD obj bin /NFL
-robocopy.exe .\lib\DirectXDependencies\  .\artifacts\srcdist\lib\DirectXDependencies\ /S /NFL
-robocopy.exe .\lib\vcredistDependencies\ .\artifacts\srcdist\lib\vcredistDependencies\ /S /NFL
-robocopy.exe .\docs\                     .\artifacts\srcdist\docs\ /S /NFL
-Compress-Archive .\artifacts\srcdist\ .\artifacts\EMU7800.src.zip -CompressionLevel Optimal -Force
+robocopy.exe .\src\                                      .\artifacts\EMU7800.src\src\ *.cs *.csproj *.cpp *.h *.manifest *.sln  *.filters *.rc *.vcxproj *.ico *.txt *.png *.json /S /XD obj bin /NFL
+robocopy.exe .\lib\DirectXDependencies\                  .\artifacts\EMU7800.src\lib\DirectXDependencies\ /S /NFL
+robocopy.exe .\lib\vcredistDependencies\                 .\artifacts\EMU7800.src\lib\vcredistDependencies\ /S /NFL
+robocopy.exe .\docs\                                     .\artifacts\EMU7800.src\docs\ /S /NFL
+Compress-Archive .\artifacts\EMU7800.src\                .\artifacts\EMU7800.src.zip -CompressionLevel Optimal -Force
 
 # copy executable artifacts
-robocopy.exe     .\src\shell\bin\Release\net5.0\win-x64\ .\artifacts\bindist\ *.dll *.exe /S /NFL
-New-Item -Name   .\artifacts\bindist\ROMS\ -ItemType directory
-Compress-Archive .\lib\roms\Bios78      .\artifacts\bindist\ROMS\Bios78.zip      -CompressionLevel Optimal -Force
-Compress-Archive .\lib\roms\HomeBrews26 .\artifacts\bindist\ROMS\HomeBrews26.zip -CompressionLevel Optimal -Force
-Compress-Archive .\lib\roms\HomeBrews78 .\artifacts\bindist\ROMS\HomeBrews78.zip -CompressionLevel Optimal -Force
-robocopy.exe     .\lib\vcredistDependencies\x64\ .\artifacts\bindist\ msvcp120.dll
+robocopy.exe     .\src\shell\bin\Release\net5.0\win-x64\ .\artifacts\EMU7800.bin\ *.dll *.exe /S /NFL
+New-Item -Name                                           .\artifacts\EMU7800.bin\ROMS\ -ItemType directory
+Compress-Archive .\lib\roms\Bios78\                      .\artifacts\EMU7800.bin\ROMS\Bios78.zip      -CompressionLevel Optimal -Force
+Compress-Archive .\lib\roms\HomeBrews26\                 .\artifacts\EMU7800.bin\ROMS\HomeBrews26.zip -CompressionLevel Optimal -Force
+Compress-Archive .\lib\roms\HomeBrews78\                 .\artifacts\EMU7800.bin\ROMS\HomeBrews78.zip -CompressionLevel Optimal -Force
+robocopy.exe     .\lib\vcredistDependencies\x64\         .\artifacts\EMU7800.bin\ msvcp120.dll
+Compress-Archive .\artifacts\EMU7800.bin\                .\artifacts\EMU7800.bin.zip
 
 Pop-Location
