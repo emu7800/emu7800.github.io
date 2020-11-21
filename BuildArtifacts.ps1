1628a46238c7057eaa28b4f183653ca9564ddf89
Mike Murphy <mike@jones-murphy.org>
2020-11-21 13:15:42 -0800

    project config change

--- BuildArtifacts.ps1
+++ BuildArtifacts.ps1
@@ -11,12 +11,12 @@ robocopy.exe .\docs\                                     .\artifacts\EMU7800.src
 Compress-Archive .\artifacts\EMU7800.src\                .\artifacts\EMU7800.src.zip -CompressionLevel Optimal -Force
 
 # copy executable artifacts
-robocopy.exe     .\src\shell\bin\Release\net5.0\win-x64\ .\artifacts\EMU7800.bin\ *.dll *.exe /S /NFL
+robocopy.exe     .\src\shell\bin\Release\net5.0\         .\artifacts\EMU7800.bin\ /S /NFL
+robocopy.exe     .\src\shell\                            .\artifacts\EMU7800.bin\ EMUIcon.ico /NFL
 New-Item -Name                                           .\artifacts\EMU7800.bin\ROMS\ -ItemType directory
 Compress-Archive .\lib\roms\Bios78\                      .\artifacts\EMU7800.bin\ROMS\Bios78.zip      -CompressionLevel Optimal -Force
 Compress-Archive .\lib\roms\HomeBrews26\                 .\artifacts\EMU7800.bin\ROMS\HomeBrews26.zip -CompressionLevel Optimal -Force
 Compress-Archive .\lib\roms\HomeBrews78\                 .\artifacts\EMU7800.bin\ROMS\HomeBrews78.zip -CompressionLevel Optimal -Force
 robocopy.exe     .\lib\vcredistDependencies\x64\         .\artifacts\EMU7800.bin\ msvcp120.dll
-Compress-Archive .\artifacts\EMU7800.bin\                .\artifacts\EMU7800.bin.zip
 
 Pop-Location
