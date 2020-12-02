5e1b1c71a48620fdbc678124b25a29101acfcdf9
Mike Murphy <mike@jones-murphy.org>
2020-12-01 16:12:47 -0800

    build script update

--- BuildArtifacts.ps1
+++ BuildArtifacts.ps1
@@ -1,5 +1,11 @@
 Push-Location $PSScriptRoot
 
+Push-Location .\src\win32\
+
+dotnet.exe publish -p:PublishProfile=FolderProfile
+
+Pop-Location
+
 # clear target
 Remove-Item -Recurse -Force .\artifacts
 
@@ -9,7 +15,7 @@ robocopy.exe .\docs\                                     .\artifacts\EMU7800.src
 Compress-Archive .\artifacts\EMU7800.src\                .\artifacts\EMU7800.src.zip -CompressionLevel Optimal -Force
 
 # copy executable artifacts
-robocopy.exe     .\src\win32\bin\Release\net5.0\publish\ .\artifacts\EMU7800.bin\ /XD ref /XF *.runtimeconfig.dev.json *.exp *.ilk *.lib *.pdb /S /NFL
+robocopy.exe     .\src\win32\bin\Release\net5.0\publish\ .\artifacts\EMU7800.bin\ /XD ref /XF *.runtimeconfig.dev.json *.exp *.ilk *.lib *.pdb *.iobj *.ipdb  /S /NFL
 New-Item -Name                                           .\artifacts\EMU7800.bin\ROMS\ -ItemType directory
 Compress-Archive .\lib\roms\Bios78\                      .\artifacts\EMU7800.bin\ROMS\Bios78.zip      -CompressionLevel Optimal -Force
 Compress-Archive .\lib\roms\HomeBrews26\                 .\artifacts\EMU7800.bin\ROMS\HomeBrews26.zip -CompressionLevel Optimal -Force
