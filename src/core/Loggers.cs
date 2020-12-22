3c7d6802374735b7c29a696afcb4b3064563688f
Mike Murphy <mike@jones-murphy.org>
2020-12-22 15:26:13 -0800

    loggers update

---
+++ src/core/Loggers.cs
@@ -0,0 +1,26 @@
+﻿namespace EMU7800.Core
+{
+    public class NullLogger : ILogger
+    {
+        public static readonly ILogger Default = new NullLogger();
+
+        public void WriteLine(string message) {}
+
+        public void Write(string message) {}
+
+        private NullLogger() {}
+    }
+
+    public class DebugLogger : ILogger
+    {
+        public static readonly ILogger Default = new DebugLogger();
+
+        public void WriteLine(string message)
+            => System.Diagnostics.Debug.WriteLine(message);
+
+        public void Write(string message)
+            => System.Diagnostics.Debug.Write(message);
+
+        private DebugLogger() {}
+    }
+}
